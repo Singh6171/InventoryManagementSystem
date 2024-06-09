191292c3c9c68bb67e0522c9a1a7fe1ea2e4c8a9
singh6171 <singh6171@protonmail.com>
2024-06-08 19:17:00 -0600

    full IMS project

--- src/dao/ConnectionProvider.java
+++ src/dao/ConnectionProvider.java
@@ -14,13 +14,13 @@ public class ConnectionProvider {
     
    public static Connection getCon() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); // Corrected driver class name
+            Class.forName("com.mysql.cj.jdbc.Driver"); // driver class name
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/inventory?useSSL=false&serverTimezone=UTC", "root", "Tiwana@6171");
             return con;
             
         } catch (Exception e) {
             System.out.println("Error establishing database connection: " + e.getMessage());
-            e.printStackTrace(); // Log the full stack trace for debugging
+            e.printStackTrace(); 
             return null;
         }
     }
